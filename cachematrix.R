f2964323643a582d634fe02318c14db50d3f3db1
sahil.chug <sahil.chug@fireeye.com>
2014-12-09 19:51:08 +0530

    added some test and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## makeCacheMatrix creates a matrix with
+## makeCacheMatrix creates a special matrix object that caches an inverse of matrix
+## the object 4 methods get, set, getinverse, setinverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        # intitalize inv to NULL
 	inv <- NULL
         set <- function(y){
             x <<- y
             inv <<- NULL
 	}
-        get <- function() x
-	setinverse <- function(inverse) inv <<- inverse
-	getinverse <- function() inv
+        get <- function() x  # return the matrix
+	setinverse <- function(inverse) inv <<- inverse  # assign inverse to inv
+	getinverse <- function() inv  # return the inverse
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns a matrix which is inverse of x
+## The function does a check if the inverse already exists
+## in such a case it will print out "getting cached inverse of matrix"
+## and return inverse of matrix
+## if the inverse is not cached the function uses solve to compute the inverse
+## and then assign it to x using setinverse property
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -31,3 +38,34 @@ cacheSolve <- function(x, ...) {
         x$setinverse(inv)
         inv
 }
+
+# tests on R command line that the code works as expected
+c = rbind(c(1, -1/4), c(-1/4, 1))
+test <- makeCacheMatrix()
+
+test$set(c)
+test$get()
+
+# output
+#       [,1]  [,2]
+# [1,]  1.00 -0.25
+# [2,] -0.25  1.00
+
+inverse <- cacheSolve(test)
+
+# output
+#          [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+
+# test caching 
+inverse <- cacheSolve(test)
+
+# output
+# getting cached inverse of matrix
+
+inverse
+# output
+#          [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
